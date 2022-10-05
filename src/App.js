cb2702e3ab79c6f24e9514506e93bda20dd11cd3
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-05 23:09:55 +0530

    updated app with routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes,Route} from 'react-router-dom';
 import './App.css';
-
+import Index from './components';
+import About from './components/About';
+import Footer from './components/Footer';
+import NavBar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+      <Routes>
+        <Route path="/" element={<Index />}/>
+        <Route path="/about" element={<About />}/>
+        
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
