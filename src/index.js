69032ff7f5f9f9bdddd6e8a61b0a62ce8c6f84ec
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-05 23:10:10 +0530

    updated index with app

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ import About from './components/About';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar/>
-    <About/>
-    <Footer/>
+    <App/>
   </React.StrictMode>
 );
 
