1c1c99f68b53dfcbfdf44224332301ee6a144a2c
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-05 22:42:17 +0530

    modified index

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import NavBar from './components/Navbar';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
+import About from './components/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavBar/>
+    <About/>
+    <Footer/>
   </React.StrictMode>
 );
 
