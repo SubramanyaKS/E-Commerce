12ae3b50796af4c4aa4f3bf67c95d307668b4b8f
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-05 22:42:40 +0530

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-
+import {FaShoppingCart} from '@react-icons/all-files/fa/FaShoppingCart'
 
 const NavBar =()=>{
     return(
         
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand style={{fontFamily:"cursive"}} href="/home">Namma kart</Navbar.Brand>
+                
+                <Navbar.Brand style={{fontFamily:"cursive"}} href="/home"><FaShoppingCart style={{ color: 'white'}}/> Namma nadu kart</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end">
           <Nav>
