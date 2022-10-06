20a215aa91fa65ffabdfd216eb0bfc53ce8b5cf8
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-06 17:36:57 +0530

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import {FaShoppingCart} from '@react-icons/all-files/fa/FaShoppingCart'
-
+import {FcSearch} from '@react-icons/all-files/fc/FcSearch'
 const NavBar =()=>{
     return(
         
@@ -10,7 +12,17 @@ const NavBar =()=>{
             <Container>
                 
                 <Navbar.Brand style={{fontFamily:"cursive"}} href="/home"><FaShoppingCart style={{ color: 'white'}}/> Namma nadu kart</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                
+                <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success"><FcSearch/></Button>
+          </Form>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end">
           <Nav>
             <Nav.Link href="/about">About</Nav.Link>
@@ -20,6 +32,7 @@ const NavBar =()=>{
           </Nav>
           
         </Navbar.Collapse>
+        
             </Container>
         </Navbar>
         
