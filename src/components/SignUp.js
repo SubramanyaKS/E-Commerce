5160657c4b25aeb4b2d49f72286141bb803c7e6a
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-07 19:35:21 +0530

    signup

---
+++ src/components/SignUp.js
@@ -0,0 +1,41 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Card } from "react-bootstrap";
+import './css/Login.css';
+import { Row } from "react-bootstrap";
+import {FaUser} from '@react-icons/all-files/fa/FaUser';
+
+const SignUp = () => {
+  return (
+    <div className="login">
+        <h3 style={{textAlign:"center"}}>Register User</h3><br/>
+        <Row className="justify-content-center">
+      <Card  border="primary" style={{ width: '20em',backgroundColor:"aqua",alignContent:"center" }}>
+        <Card.Body>
+        <FaUser style={{fontSize:"50px", alignContent:"center",marginLeft:"45%" }}/><br/><br/>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name:</Form.Label>
+              <Form.Control type="email" placeholder="Enter Name" />
+              <Form.Text className="text-muted"></Form.Text>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button className="button" variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      </Row>
+    </div>
+  );
+};
+export default SignUp;
