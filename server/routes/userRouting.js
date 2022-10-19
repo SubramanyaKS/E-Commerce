04a7b1d8fc1d4dbee603caa75a9d2838fc1f9f44
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-19 17:10:51 +0530

    updated routing

---
+++ server/routes/userRouting.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userController= require('../controllers/userController');
+const userrouting = express.Router();
+
+userrouting.get('/all', userController.getUser);
+userrouting.post('/', userController.newUser);
+userrouting.put('/:id', userController.updateUser);
+userrouting.get('/:id', userController.getUserId);
+userrouting.post('/login', userController.userLogin);
+userrouting.all('*', userController.invalid);
+
+module.exports = userrouting;
