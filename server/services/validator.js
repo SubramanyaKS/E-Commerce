a0dc97aa28a692a135e73a7e4022f4c979e42c39
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-19 17:11:37 +0530

    update server

---
+++ server/services/validator.js
@@ -0,0 +1,56 @@
+exports.ValidateName = function (name) {
+    if (name.trim().length > 2 && name.trim().length<51) {
+      console.log(name);
+      return true;
+    }
+    return false;
+  };
+// eslint-disable-next-line no-undef
+  exports.ValidateAge = function (date) {
+    //console.log(date);
+    var today = new Date();
+    var birthDate =new Date(date);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    if (age > 20 && age<100) {
+      return true;
+    }
+    return false;
+  };
+// eslint-disable-next-line no-undef
+exports.ValidatePassword = function (password) {
+  if (password.trim().length >= 5 && password.trim().length<=10) {
+    console.log(password);
+    return true;
+  }
+  return false;
+};
+// eslint-disable-next-line no-undef
+exports.ValidatePhoneNo = function (number) {
+  if (number.trim().length == 10) {
+    //console.log(number);
+    return true;
+  }
+  return false;
+};
+// eslint-disable-next-line no-undef
+exports.ValidatePincode = function (number) {
+  var pinno = number.toString();
+  if (pinno.trim().length==6) {
+    //console.log(number);
+    return true;
+  }
+  return false;
+};
+// eslint-disable-next-line no-undef
+exports.ValidateGender = function (gender) {
+  console.log(gender);
+  if (gender=="M"|| gender=="F") {
+    //console.log(password);
+    return true;
+  }
+  return false;
+};
