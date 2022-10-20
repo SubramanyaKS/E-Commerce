2d86742a692bc72ff52dd3449351d56eb02974f0
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-20 07:08:18 +0530

    added

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes,Route} from 'react-router-dom';
 import './App.css';
+import Index from './components';
+import About from './components/About';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import NavBar from './components/Navbar';
+import SignUp from './components/SignUp';
+import UserSucess from './components/UserSucess';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+      <Routes>
+        <Route path="/" element={<Index />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<SignUp />}/>
+        <Route path="/usersucess" element={<UserSucess />}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
