a8fd15526306c45227391cc596bb36fcad06ea15
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-20 07:09:22 +0530

    added

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const errorLogger = require('./services/errorLogger');
 const userroute= require('./routes/userRouting');
-
+const cors = require('cors');
 const app= express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/users', userroute);
 app.get("/",(req,res)=>{
@@ -21,7 +28,7 @@ app.all('*', (req, res, next) => {
     next(err);
   });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
