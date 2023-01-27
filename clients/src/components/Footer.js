487d1e745d8446f7058edf322a927980800dd872
Subramanya K S <subramanyaks2205@gmail.com>
2023-01-27 19:43:23 +0530

    updated footer

--- clients/src/components/Footer.js
+++ clients/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
             <Container>
                 <Row>
-                    <Col className='text-center py-3'>Copyright &copy; Subramanya</Col>
+                    <Col className='text-center py-3'>Copyright &copy; 2023, Namma nadu kart</Col>
                 </Row>
             </Container>
         </footer>
