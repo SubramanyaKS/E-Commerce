46fe7ce1c3f8e3199048aeee5e5358fbecd39a8b
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-04 17:30:16 +0530

    updated navbar

--- clients/src/components/Navbar.jsx
+++ clients/src/components/Navbar.jsx
@@ -6,12 +6,11 @@ import Button from 'react-bootstrap/Button';
 import {FaShoppingCart} from '@react-icons/all-files/fa/FaShoppingCart'
 import {FcSearch} from '@react-icons/all-files/fc/FcSearch';
 import 'bootstrap/dist/css/bootstrap.css';
+import { LogoutS } from '../util/helper';
+
 const NavBar =()=>{
-  /*const LogoutU=()=>{
-    sessionStorage.removeItem('uid');
-    sessionStorage.removeItem('uAuthenticated');
-    window.location.href = "/home";
-}*/
+  let sa = sessionStorage.getItem('sid');
+ 
     return(
         
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -31,11 +30,18 @@ const NavBar =()=>{
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end">
           <Nav>
-            <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+            {sa?<>
+              <Nav.Link style={{textDecoration: "none"}}  onClick={LogoutS}> Log Out </Nav.Link>
+            </>:<>
+            
             <Nav.Link href="/login">Login</Nav.Link>
             <Nav.Link href="/signup">Signup</Nav.Link>
             <Nav.Link href="/seller">Seller</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            
+            </>
+            }
           </Nav>
           
         </Navbar.Collapse>
