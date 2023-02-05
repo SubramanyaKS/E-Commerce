d8421a059202f7b8fad9f3f77118a6f6e04852eb
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-05 13:16:14 +0530

    updated seller home

--- clients/src/pages/seller/SellerHome.jsx
+++ clients/src/pages/seller/SellerHome.jsx
@@ -1,17 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+// import { fetchProduct } from "../../util/helper";
+import axios from "axios";
+//import Item from "../../components/Item";
+import Display from "./Display";
+
 const SellerHome=()=>{
+    const[prod,setProd] = useState([]);
     const navigate= new useNavigate();
     useEffect(()=>{
+       
         let p = sessionStorage.getItem("sAuthenticated");
-    if(!p){
-        navigate("/seller-login")
+        if(!p){
+        navigate("/seller-login");
     }
-
+    else{
+      axios.get("http://localhost:4000/products")
+      .then((response) => {
+        console.log(response.data);
+        setProd(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+    
     });
     
     return(<>
-    <p>Hi</p>
+    <p>Hi Seller</p>   
+        <div style={{ display: "flex", flexWrap: "wrap"}}>
+        {prod.map((article) => {
+          return (
+            <Display
+              name={article.name}
+              price={article.price}
+              category={article.category}
+              color={article.color}
+              description={article.description}
+              image={article.image}
+              id={article.id}
+            />
+          );
+        })}
+       </div>
     </>);
 }
 export default SellerHome;
