52e31b98757d324eae96bc22372c4a137feb6e86
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-05 07:35:46 +0530

    updated add product

--- clients/src/components/AddProduct.jsx
+++ clients/src/components/AddProduct.jsx
@@ -3,7 +3,45 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Card } from "react-bootstrap";
 import { Row } from 'react-bootstrap';
+import { useState  , useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+
 const AddProduct = () => {
+  const navigate = useNavigate();
+const [product,setProduct]= useState({
+  name:"",
+  price:"",
+  category: "",
+  color: "",
+  description:"",
+  image:"",
+})
+useEffect(() => {
+  axios
+    .get("http://localhost:4000/products")
+    .then((response) => {
+      setProduct(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+
+  const ProductAdd= async (event)=>{
+    event.preventDefault();
+    await axios.post("http://localhost:4000/products/",product)
+    .then((res)=>{
+        navigate('/seller');
+      }
+      
+    )};
+    const handleChange = (event) => {
+      setProduct({ ...product, [event.target.name]: event.target.value });
+    };
+
   return (
     <div className="product-form">
         <h3 style={{textAlign:"center"}}>Add Product</h3><br/>
@@ -17,39 +55,39 @@ const AddProduct = () => {
           }}
         >
             <Card.Body>
-        <Form>
+        <Form onSubmit={ProductAdd}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Product name</Form.Label>
-        <Form.Control name="name" type="text" placeholder="Enter Name" />
+        <Form.Control name="name" value={product.name} onChange={handleChange} type="text" placeholder="Enter Name" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Price</Form.Label>
-        <Form.Control name="price" type="text" placeholder="Enter Price" />
+        <Form.Control name="price" value={product.price} onChange={handleChange} type="text" placeholder="Enter Price" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Category</Form.Label>
-        <Form.Control name="category" type="text" placeholder="Enter category" />
+        <Form.Control name="category" onChange={handleChange} value={product.category} type="text" placeholder="Enter category" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Color</Form.Label>
-        <Form.Control name="color" type="text" placeholder="Enter color" />
+        <Form.Control name="color" value={product.color} onChange={handleChange} type="text" placeholder="Enter color" />
       </Form.Group>
 
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Description</Form.Label>
-        <Form.Control name="description" as="textarea" rows={3} />
+        <Form.Control name="description"  onChange={handleChange} value={product.description} as="textarea" rows={3} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Image Link</Form.Label>
-        <Form.Control name="image" type="text" placeholder="Enter link" />
+        <Form.Control name="image" value={product.image} onChange={handleChange} type="text" placeholder="Enter link" />
       </Form.Group>
 
-      <Button className="text-center" variant="primary" type="submit">
+      <Button className="text-center" variant="primary" value="Submit" type="submit">
         Submit
       </Button>
     </Form>
