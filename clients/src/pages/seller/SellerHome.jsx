843be54e775cbd9ba8e66b8aba275216e02206bc
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-06 16:48:15 +0530

    updated

--- clients/src/pages/seller/SellerHome.jsx
+++ clients/src/pages/seller/SellerHome.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './seller.css';
 // import { fetchProduct } from "../../util/helper";
 import axios from "axios";
 //import Item from "../../components/Item";
-import Display from "./Display";
 
 const SellerHome=()=>{
     const[prod,setProd] = useState([]);
@@ -12,13 +14,14 @@ const SellerHome=()=>{
        
         let p = sessionStorage.getItem("sAuthenticated");
         if(!p){
-        navigate("/seller-login");
-    }
-    else{
-      axios.get("http://localhost:4000/products")
-      .then((response) => {
+          navigate("/seller-login");
+        }
+      else{
+        axios.get("http://localhost:4000/products")
+        .then((response) => {
         console.log(response.data);
         setProd(response.data);
+        
       })
       .catch((error) => {
         console.log(error);
@@ -26,23 +29,42 @@ const SellerHome=()=>{
     }
     
     });
+
+    const deleteProduct =(id)=>{
+      let bId = parseInt(id);
+      axios.delete('http://localhost:4000/products/'+bId)
+      .then((res)=>{
+        axios.get('http://localhost:4000/products')
+        .then((res)=>{setProd(res.body);})
+          
+      });
+    }
+    
     
     return(<>
     <p>Hi Seller</p>   
         <div style={{ display: "flex", flexWrap: "wrap"}}>
-        {prod.map((article) => {
-          return (
-            <Display
-              name={article.name}
-              price={article.price}
-              category={article.category}
-              color={article.color}
-              description={article.description}
-              image={article.image}
-              id={article.id}
-            />
-          );
-        })}
+        {prod.map((article) => (
+          <div className="mb-3" key={article.id}>
+            <Card style={{ width: '25rem', height:'35rem' }}>
+            <Card.Img style={{height:'10rem'}} variant="top" src={article.image} />
+          <Card.Body>
+            {/* {image} */}
+          <Card.Title>{article.name}</Card.Title>
+          <br/>
+          <Card.Text>
+            <b>Description:</b>{article.description}
+          </Card.Text>
+          <Card.Text><b>Category: </b>{article.category}</Card.Text>
+          <Card.Text><b>Color: </b>{article.color}</Card.Text>
+          <Card.Text><b>Price: </b>{article.price}</Card.Text>
+          <Button onClick={()=>deleteProduct(article.id)} variant="primary">Delete</Button>
+          
+        </Card.Body>
+      </Card>
+          </div>
+          
+        ))}
        </div>
     </>);
 }
