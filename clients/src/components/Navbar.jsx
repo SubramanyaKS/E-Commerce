2395589421e7909e19e059b10ccd7eda9aa2c6f0
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-09 20:01:49 +0530

    updated navbar

--- clients/src/components/Navbar.jsx
+++ clients/src/components/Navbar.jsx
@@ -10,10 +10,11 @@ import { LogoutS } from '../util/helper';
 
 const NavBar =()=>{
   let sa = sessionStorage.getItem('sid');
+  let ua = sessionStorage.getItem('uid');
  
     return(
         
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 
                 <Navbar.Brand style={{fontFamily:"cursive"}} href="/home"><FaShoppingCart style={{ color: 'white'}}/> Namma nadu kart</Navbar.Brand>
@@ -30,13 +31,17 @@ const NavBar =()=>{
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end">
           <Nav>
+          <Nav.Link href="/home">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
+            
             {sa?<>
             <Nav.Link href="/addproduct">Add Product</Nav.Link>
               <Nav.Link style={{textDecoration: "none"}}  onClick={LogoutS}> Log Out </Nav.Link>
             </>:<>
-            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/carts">Carts</Nav.Link>
+            <Nav.Link href="/items" >Item</Nav.Link>
+            {/* <Nav.Link href="/cart">Cart <p  style={{display:'inline-flex'}}className="add-cart">3</p></Nav.Link> */}
             <Nav.Link href="/login">Login</Nav.Link>
             <Nav.Link href="/signup">Signup</Nav.Link>
             <Nav.Link href="/seller">Seller</Nav.Link>
