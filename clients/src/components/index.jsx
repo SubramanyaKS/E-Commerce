e232a0c0df83fa233f0f9018c311b6a5faf01396
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-14 17:03:15 +0530

    updated

--- clients/src/components/index.jsx
+++ clients/src/components/index.jsx
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from 'axios';
-import Item from "./Item";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './css/index.css';
+import { useNavigate } from "react-router";
+import {useCart } from "react-use-cart";
+import CartItem from "./CartItem";
+
 
 
 const Index = ()=>{
     const [product,setProduct] =useState([]);
-
+    let quantity=0;
     // useEffect(()=>{
     //     axios.get("http://localhost:4000/products")
     //     .then((res)=>{
@@ -32,23 +38,50 @@ const Index = ()=>{
         console.log("News",news());
       }, []);
 
+      const navigate= new useNavigate();
+  const { addItem } = useCart();
+  
+  const AddCart=({article})=>{
+    let u = sessionStorage.getItem("uAuthenticated");
+    let ua =Boolean(u);
+    if(ua){
+      return(<CartItem article={article} />);
+      //addItem(article);
+    }
+    else{
+      navigate("/login");
+    }
+    
+  }
+
     return(
         <>
         <h1 style={{textAlign:"center"}}>Welcome to Namma Nadu Kart</h1>
         <div style={{ display: "flex", flexWrap: "wrap"}}>
-        {product.map((article) => {
-          return (
-            <Item
-              name={article.name}
-              price={article.price}
-              category={article.category}
-              color={article.color}
-              description={article.description}
-              image={article.image}
-              id={article.id}
-            />
-          );
-        })}
+        {product.map((article) => (
+         
+         <div className="mb-3" key={article.id}>
+            <Card style={{ width: '25rem', height:'35rem' }}>
+            <Card.Img style={{height:'10rem'}} variant="top" src={article.image} />
+          <Card.Body>
+            {/* {image} */}
+          <Card.Title>{article.name}</Card.Title>
+          <br/>
+          <Card.Text>
+            <b>Description:</b>{article.description}
+          </Card.Text>
+          <Card.Text><b>Category: </b>{article.category}</Card.Text>
+          <Card.Text><b>Color: </b>{article.color}</Card.Text>
+          <Card.Text><b>Price: Rs. </b>{article.price}</Card.Text>
+          <input text="number" value={quantity} name="quantity" /><br></br>
+          {quantity===0?
+          <Button value="Submit" type="submit" onClick={()=>AddCart(article)} variant="primary">Add to Cart</Button>
+          :null}
+        </Card.Body>
+      </Card>
+      
+          </div>
+        ))}
        </div>
         
         </>
