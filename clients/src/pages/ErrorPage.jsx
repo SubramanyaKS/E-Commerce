268f62ae3367abc80d6a67c48603226f2becf41d
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-14 19:47:03 +0530

    updated error

---
+++ clients/src/pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+const ErrorPage = () => {
+  return (
+    <div className="error">
+      <h2>404 ERROR</h2>
+      <h3>Page Not Found</h3>
+    </div>
+  )
+}
+
+export default ErrorPage
