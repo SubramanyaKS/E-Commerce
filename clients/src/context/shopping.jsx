72929b75cb6a683eb4975d69a605cc4d9185ab41
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-15 22:43:42 +0530

    updated

--- clients/src/context/shopping.jsx
+++ clients/src/context/shopping.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, {createContext, useState} from 'react'
 
-const Shopping = () => {
+export const ShopContext = createContext(null);
+
+export const ShoppingCartProvider = (props) => {
+  const [cartItems, setCartItems] = useState([]);
+  const [productItems, setProductItems] = useState([]);
+  
   return (
-    <div>
-      
-    </div>
+    <ShopContext.Provider value={{cartItems,setCartItems,productItems,setProductItems}}>
+      {props.children}
+    </ShopContext.Provider>
   )
 }
-
-export default Shopping;
