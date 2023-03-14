fcbb4fa1462aa7865c9087cc101f45881168c715
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-14 22:53:07 +0530

    update some changes

---
+++ clients/src/old/CartItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CartItem = ({state}) => {
+  return (
+    <div>
+      {/* {article.map((cart)=>( */}
+        <p>{StaticRange}</p>
+      {/* ))} */}
+    </div>
+  )
+}
+
+export default CartItem;
