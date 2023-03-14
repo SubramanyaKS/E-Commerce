f22a864268735de34ffd4dc252861c0ca8a497d9
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-14 22:52:09 +0530

    update some changes

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,42 +1,13 @@
-import { BrowserRouter, Routes,Route} from 'react-router-dom';
 import './App.css';
-import Index from './components';
-import About from './pages/About';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import NavBar from './components/Navbar';
-import SignUp from './components/SignUp';
-import SellerHome from './pages/seller/SellerHome';
-import UserSucess from './components/UserSucess';
-import 'bootstrap/dist/css/bootstrap.css';
-import SellerLogin from './pages/seller/SellerLogIn';
-import SellerSignUp from './pages/seller/SellerSignUp';
-import AddProduct from './pages/AddProduct';
-import Cart from './components/Cart';
-import Carts from './components/Carts';
-import CartItem from './components/CartItem';
+import Routing from './Routing';
+import { ShoppingCartProvider } from './context/shopping';
+// import { ShopContext } from './context/shopping';
 function App() {
   return (
-    <BrowserRouter>
-    <NavBar/>
-      <Routes>
-        <Route path="/carts" element={<Carts/>} />
-        <Route path="/" element={<Index />}/>
-        <Route path="/home" element={<Index />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/signup" element={<SignUp />}/>
-        <Route path="/usersucess" element={<UserSucess />}/>
-        <Route path="/seller" element={<SellerHome/>}/>
-        <Route path="/seller-login" element={<SellerLogin/>}/>
-        <Route path="seller-signup" element={<SellerSignUp/>} />
-        <Route path="/signup" element={<SignUp />}/>
-        <Route path="/addproduct" element={<AddProduct />}/>
-        <Route path="/cart" element={<Cart />}/>
-        <Route path="/items" element={<CartItem/>} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <Routing/>
+    </ShoppingCartProvider>
+    
   );
 }
 
