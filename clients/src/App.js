a8397760a44b5ef929b85463f786876ae6081f48
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-15 11:08:48 +0530

    restarting code with different method

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
 import Routing from './Routing';
-import { ShoppingCartProvider } from './context/shopping';
-// import { ShopContext } from './context/shopping';
 function App() {
   return (
-    <ShoppingCartProvider>
       <Routing/>
-    </ShoppingCartProvider>
     
   );
 }
