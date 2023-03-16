ab9ec7100a730412fe3a0d59afd7975e283373f6
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-16 22:03:14 +0530

    Added index

--- clients/src/index.js
+++ clients/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ShoppingCartProvider } from './context/shopping';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ShoppingCartProvider>
     <App />
-    </ShoppingCartProvider>
   </React.StrictMode>
 );
 
