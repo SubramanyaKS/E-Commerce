6e6b33e4f6337199a476b5429c8b4c02949e8b53
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-18 07:43:44 +0530

    added addto card

---
+++ clients/src/pages/AddToCart.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import { ProductState } from '../context/ProductContext';
+import { Button, Card } from 'react-bootstrap';
+// import ProductCard from '../components/ProductCard';
+
+
+const AddToCart = () => {
+  const {product} = ProductState();
+    let {id} = useParams();
+    
+  return (
+    <section>
+        <h5>Add to cart</h5>
+        {product.map((article)=>(
+            <div className="mb-3" key={article.id}>
+          {String(article.id)===id?
+          <Card>
+            <Card.Body style={{flexDirection:"row"}}>
+              <Card.Text>{article.name}</Card.Text>
+              <Card.Text>{article.price}</Card.Text>
+              <div style={{flexDirection:"row"}}>
+                <Button>+</Button>
+                <Button>-</Button>
+              </div>
+            </Card.Body>
+          </Card>
+          :null}
+          </div>))}
+        {/* <ProductCard article={data}/> */}
+        
+    </section>
+  )
+}
+
+export default AddToCart;
