973fbde696e1e46c16f3f1e13a579cce7cb958f2
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-18 07:43:01 +0530

    added single product

---
+++ clients/src/pages/SingleProduct.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useNavigate, useParams } from "react-router";
+import '../assets/css/index.css';
+import { ProductState } from '../context/ProductContext';
+import ProductCard from '../components/ProductCard';
+
+const SingleProduct = () => {
+    const {id}= useParams();
+    // console.log(typeof id," ",id);
+    const {product} = ProductState();
+    const navigate= useNavigate();
+  return (
+    <div>
+        {product.map((article)=>(
+            <div className="mb-3" key={article.id}>
+          <p>{String(article.id)===id?<ProductCard article={article}/>:null}</p>
+          </div>))}
+
+    </div>
+  )
+}
+
+export default SingleProduct
