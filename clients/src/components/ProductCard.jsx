933aa87994f44886fd0f10e842a56b394e86dbfb
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-25 22:29:32 +0530

    updated Product card

--- clients/src/components/ProductCard.jsx
+++ clients/src/components/ProductCard.jsx
@@ -6,17 +6,18 @@ import '../assets/css/index.css';
 const ProductCard = ({article}) => {
     const navigate= new useNavigate();
   return (
-    <Card style={{ width: '25rem', height:'35rem' }}>
+    <Card style={{ width: '25rem', height:'45rem' }}>
          <Card.Img style={{height:'10rem'}} variant="top" src={article.image} />
           <Card.Body>
-          <Card.Title>{article.name}</Card.Title>
+          <Card.Title>{article.title}</Card.Title>
           <br/>
           <Card.Text>
             <b>Description:</b>{article.description}
           </Card.Text>
           <Card.Text><b>Category: </b>{article.category}</Card.Text>
-          <Card.Text><b>Color: </b>{article.color}</Card.Text>
           <Card.Text><b>Price: Rs. </b>{article.price}</Card.Text>
+          <Card.Text><b>Rating: </b>{article.rating.rate}</Card.Text>
+          <Card.Text><b>Rating Count: </b>{article.rating.count}</Card.Text>
           <Button value="Submit" type="submit" onClick={()=>navigate(`/addtocart/${article.id}`)}>Add to Cart</Button>
         </Card.Body>
     </Card>
