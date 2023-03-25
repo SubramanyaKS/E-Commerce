ce301cbc7499af243c86a49a214fc6b7ce2e126b
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-25 22:29:44 +0530

    updated Add to card

--- clients/src/pages/AddToCart.jsx
+++ clients/src/pages/AddToCart.jsx
@@ -2,33 +2,42 @@ import React, {useState} from 'react'
 import { useParams } from 'react-router-dom';
 import { ProductState } from '../context/ProductContext';
 import { Button, Card } from 'react-bootstrap';
+import { CartState } from '../context/ShopContext';
 // import ProductCard from '../components/ProductCard';
 
 
 const AddToCart = () => {
   const [quantity, setQuantity] = useState(1);
   const {product} = ProductState();
+  const {cart,setCart} = CartState();
     let {id} = useParams();
+
+    const AddedCart=({name,price,quantity})=>{
+        setCart({articlename:name,price:price,quantity:quantity})
+        console.log(cart);
+      }
     
   return (
-    <section>
+    <section >
         <h5>Add to cart</h5>
         {product.map((article)=>(
             <div className="mb-3" key={article.id}>
           {String(article.id)===id?
-          <Card>
-            <Card.Body style={{flexDirection:"row"}}>
-              <Card.Text>{article.name}</Card.Text>
-              <Card.Text>{article.price}</Card.Text>
-              <div style={{flexDirection:"row"}}>
+          <Card style={{display:"flex",flexDirection:"row"}}>
+            <Card.Body >
+              <Card.Text><b>{article.title}</b></Card.Text>
+              <Card.Text>${article.price*quantity}</Card.Text>
+              <div style={{display:"flex",flexDirection:"row"}}>
                 <Button onClick={()=>setQuantity(quantity+1)}>+</Button>
                 <p>{quantity}</p>
-                <Button onClick={()=>setQuantity(quantity+1)}>-</Button>
+                <Button onClick={()=>setQuantity(quantity-1)}>-</Button>
               </div>
+              <Button style={{marginTop:"10px"}} onClick={()=>AddedCart(article.name,article.price,quantity)}>Added</Button>
             </Card.Body>
           </Card>
           :null}
           </div>))}
+          <p>{cart.articlename}</p>
         {/* <ProductCard article={data}/> */}
         
     </section>
