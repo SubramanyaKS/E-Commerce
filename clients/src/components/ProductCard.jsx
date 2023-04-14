305f42f552b73f15f6a4eb2aa40eacfca630de56
Subramanya K S <subramanyaks2205@gmail.com>
2023-04-14 22:20:24 +0530

    updated products

--- clients/src/components/ProductCard.jsx
+++ clients/src/components/ProductCard.jsx
@@ -1,13 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from "react-router";
-import '../assets/css/index.css';
+import '../assets/css/single.css';
 const ProductCard = ({article}) => {
     const navigate= new useNavigate();
   return (
-    <Card style={{ width: '25rem', height:'45rem' }}>
+    <Card className="shadow-lg p-3 mb-5" style={{ width: '75rem', height:'35rem' }}>
+      <Row>
+        <Col >
          <Card.Img style={{height:'10rem'}} variant="top" src={article.image} />
+         </Col>
+         <Col>
           <Card.Body>
           <Card.Title>{article.title}</Card.Title>
           <br/>
@@ -20,6 +27,8 @@ const ProductCard = ({article}) => {
           <Card.Text><b>Rating Count: </b>{article.rating.count}</Card.Text>
           <Button value="Submit" type="submit" onClick={()=>navigate(`/addtocart/${article.id}`)}>Add to Cart</Button>
         </Card.Body>
+        </Col>
+        </Row>
     </Card>
   )
 }
