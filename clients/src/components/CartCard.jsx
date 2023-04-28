c2d8b52d8a542fb16ba2312a90ca65440fc08c19
Subramanya K S <subramanyaks2205@gmail.com>
2023-04-28 22:17:35 +0530

    updated

--- clients/src/components/CartCard.jsx
+++ clients/src/components/CartCard.jsx
@@ -3,7 +3,9 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from "react-router";
 import '../assets/css/index.css';
+import { CartState } from '../context/ShopContext';
 const CartCard = ({article}) => {
+  const {count,setCount}= CartState();
     const navigate= new useNavigate();
   return (
     <Card style={{ width: '25rem', height:'35rem' }}>
@@ -17,7 +19,7 @@ const CartCard = ({article}) => {
           <Card.Text><b>Category: </b>{article.category}</Card.Text>
           <Card.Text><b>Color: </b>{article.color}</Card.Text>
           <Card.Text><b>Price: Rs. </b>{article.price}</Card.Text>
-           <Button value="Submit" type="submit" onClick={()=>navigate(`/addtocart/${article.id}`)}></Button>
+           <Button value="Submit" type="submit" onClick={()=>{setCount((prev)=>prev+1);navigate(`/addtocart/${article.id}`)}}></Button>
         </Card.Body>
     </Card>
   )
