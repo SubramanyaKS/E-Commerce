b84740ccf8c9de96ab039e390d67e5229a6bde5a
Subramanya K S <subramanyaks2205@gmail.com>
2023-07-01 11:58:58 +0530

    updated footer

--- clients/src/components/Footer.jsx
+++ clients/src/components/Footer.jsx
@@ -1,16 +1,15 @@
 import React from'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <footer>
-            <Container>
+        <footer >
                 <Row>
-                    <Col className='text-center py-3'>Copyright &copy; 2023, Namma nadu kart</Col>
+                    <Col className="text-center">
+                        <p className='text-center py-3'>Copyright &copy; 2023, Namma nadu kart</p>
+                    </Col>
                 </Row>
-            </Container>
         </footer>
     )
 }
-
 export default Footer
